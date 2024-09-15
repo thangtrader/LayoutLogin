e9c5f55646b971a754005506a0722c319b78f64e
thang <thang@gmail.com>
2024-09-15 15:09:09 +0700

    LayoutConstraint

--- app/src/main/java/com/example/layoutlogin/RegisterActivity.java
+++ app/src/main/java/com/example/layoutlogin/RegisterActivity.java
@@ -11,23 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tv_login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        TextView btn = findViewById(R.id.tv_signup);
+        TextView btn = findViewById(R.id.tv_login);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
             }
         });
     }
